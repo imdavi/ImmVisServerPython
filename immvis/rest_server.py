2d540e87a60b7d9cdc39dea197bfe15775cb4cb0
Felipe Pedroso <f.pedroso677@gmail.com>
2019-09-04 07:15:46 +0100

    Implementing rest server.

---
+++ immvis/rest_server.py
@@ -0,0 +1,12 @@
+from flask import Flask, make_response, request, jsonify
+from flask_api import status,mediatypes
+import pandas as pd
+
+_METHOD_GET = 'GET'
+_METHOD_POST = 'POST'
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Server Works!', status.HTTP_200_OK
