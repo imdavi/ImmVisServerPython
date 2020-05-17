41d46b7b724a2fc2645613b086049c659e1f09a4
Felipe Pedroso <felipe-pedroso@cookpad.com>
2020-05-17 18:37:13 +0100

    Fixing generations scripts

--- utilities/generate_python_libs.bat
+++ utilities/generate_python_libs.bat
@@ -11,10 +11,13 @@ SET PROTO_FILE=%PROTO_DIR%\immvis.proto
 
 SET IMMVIS_DIR=%ROOT_DIR%\immvis
 
+SET OUT_DIR=%IMMVIS_DIR%\rpc\proto
+MKDIR %OUT_DIR%
+
 if exist %PROTO_FILE% (
     echo Generating Python files...
 
-    python -m grpc_tools.protoc -I %PROTO_DIR%  --python_out=%IMMVIS_DIR% --grpc_python_out=%IMMVIS_DIR% %PROTO_FILE%
+    python -m grpc_tools.protoc -I %PROTO_DIR%  --python_out=%OUT_DIR% --grpc_python_out=%OUT_DIR% %PROTO_FILE%
 
     echo Done! The generated filed are available on %IMMVIS_DIR%.
 ) else (
--- utilities/generate_python_libs.sh
+++ utilities/generate_python_libs.sh
@@ -8,18 +8,18 @@ PROTO_DIR=$ROOT_DIR/proto
 
 PROTO_FILE=$PROTO_DIR/immvis.proto
 
-IMMVIS_DIR=$ROOT_DIR/immvis/proto
+OUT_DIR=$ROOT_DIR/immvis/rpc/proto
 
 if [ -f $PROTO_FILE ]; then
     echo Generating Python files...
 
-    if [ ! -e $IMMVIS_DIR ]; then
-        mkdir $IMMVIS_DIR
+    if [ ! -e $OUT_DIR ]; then
+        mkdir $OUT_DIR
     fi
 
-    python -m grpc_tools.protoc -I $PROTO_DIR  --python_out=$IMMVIS_DIR --grpc_python_out=$IMMVIS_DIR $PROTO_FILE
+    python -m grpc_tools.protoc -I $PROTO_DIR  --python_out=$OUT_DIR --grpc_python_out=$OUT_DIR $PROTO_FILE
     
-    echo Done! The generated filed are available on $IMMVIS_DIR.
+    echo Done! The generated filed are available on $OUT_DIR.
 else
    echo "The file '$PROTO_FILE' was not found."
 fi
