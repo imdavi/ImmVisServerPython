e39484a917139a303597541156f1bec01495a810
Felipe Pedroso <f.pedroso677@gmail.com>
2020-09-28 20:32:12 +0100

    Creating _get_data_set_with_selected_columns to simplify selecting columns.

--- immvis/data/data_manager.py
+++ immvis/data/data_manager.py
@@ -9,6 +9,12 @@ class DataManager():
     def __init__(self, data_frame: DataFrame = None):
         self.data_frame = data_frame
 
+    def _get_data_set_with_selected_columns(self, columns: typing.List[str]) -> DataFrame:
+        if columns is not None and len(columns) > 0:
+            return self.data_frame[columns]
+        else:
+            return self.data_frame
+
     def list_available_datasets(self) -> typing.List[str]:
         return list_available_datasets()
 
@@ -17,11 +23,8 @@ class DataManager():
         return self.data_frame
 
     def get_normalised_dataset(self, columns: typing.List[str]):
-        data_frame_to_normalise: DataFrame = None
-
-        if len(columns) > 0:
-            data_frame_to_normalise = self.data_frame[columns]
-        else:
-            data_frame_to_normalise = self.data_frame
+        data_frame_to_normalise: DataFrame = self._get_data_set_with_selected_columns(columns)
 
         return normalise_data_frame(data_frame_to_normalise)
+
+
